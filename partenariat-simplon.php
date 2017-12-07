2512bdb9deec54b277cd939071cd490dfdfb4a0c
GillesLB <deltag@laposte.net>
2017-12-07 17:53:23 +0100

    Partenariat_simplon
    version php

--- partenariat-simplon.php
+++ partenariat-simplon.php
@@ -1,10 +1,16 @@
 <?php
 /*
-
+Plugin Name: simplon
+Plugin URI: https://simplon.co/
+Description: Merci Simplon
+Version: 0.1
+Author: Deltag
+Author URI: 2d4208ba105d4e2285387a76737d5a3f.yatu.ws
+License: GPL2
 */
-
-
-
-
-
-?>
+function SimplonShortcode() {
+	return '<p>La publication de cet article est possible grâce à mon super partenaire <a href="https://simplon.co">simplon.co</a> - une entreprise de
+l’économie sociale et solidaire et un réseau de « fabriques » (écoles) qui propose
+des formations GRATUITES pour devenir développeur web.</p>';
+}
+add_shortcode('simplon', 'SimplonShortcode');
